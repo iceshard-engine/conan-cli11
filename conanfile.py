dad6bf0a02610ea9466ca755910dfe5697f3609c
dandielo <daniel.penkala@gmail.com>
2020-09-19 00:17:09 +0200

    Updated to version 1.9.1

--- conanfile.py
+++ conanfile.py
@@ -1,28 +1,28 @@
-from conans import ConanFile, MSBuild, tools
-from shutil import copyfile
+from conans import ConanFile
 import os
 
 class CLI11Conan(ConanFile):
     name = "CLI11"
-    version = "1.8.0"
     license = "https://github.com/CLIUtils/CLI11/blob/master/LICENSE"
     description = "CLI11 is a command line parser for C++11 and beyond that provides a rich feature set with a simple and intuitive interface."
     url = "https://github.com/CLIUtils/CLI11"
 
-    CLI_FOLDER_NAME = "cli-{}".format(version)
+    # Iceshard conan tools
+    python_requires = "conan-iceshard-tools/0.5.4@iceshard/stable"
+    python_requires_extend = "conan-iceshard-tools.IceTools"
+
+    settings = "compiler"
 
     def package_id(self):
         self.info.header_only()
 
-    def source(self):
-        git = tools.Git(folder=self.CLI_FOLDER_NAME)
-        git.clone("https://github.com/CLIUtils/CLI11.git")
-        git.checkout("master")
+    def init(self):
+        self.ice_init("none")
 
     def package(self):
-        self.copy("*", src=os.path.join(self.CLI_FOLDER_NAME, "include"), dst="include")
-        self.copy("LICENSE", src=self.CLI_FOLDER_NAME)
-        self.copy("README.md", src=self.CLI_FOLDER_NAME)
+        self.copy("*", src=os.path.join(self._ice.out_dir, "include"), dst="include")
+        self.copy("LICENSE", src=self._ice.out_dir)
+        self.copy("README.md", src=self._ice.out_dir)
 
     def package_info(self):
         self.cpp_info.includedirs = ["include"]
